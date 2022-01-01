ef9b631b78059e434797a855f6804debaa94108b
David CM <dhf360@gmail.com>
2021-12-31 20:22:29 -0600

    modified buildVars to work with workflows

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""This add-on beeps with some keyboard events."""),
 	# version
-	"addon_version" : "1.7-dev",
+	"addon_version" : ,
 	# Author(s)
 	"addon_author" : u"David CM <dhf360@gmail.com>",
 	# URL for the add-on documentation support
